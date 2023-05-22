314b322bffaeee5f63b6a10ac5736df1dbeea5c9
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-05-22 19:54:34 +0200

    add NoteList page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import NewNote from './NewNote'
 import { useLocalStorage } from './useLocalStorage'
 import { useMemo } from 'react'
 import {v4 as uuidV4} from 'uuid'
+import NoteList from './NoteList'
 
 export type Note = {
   id: string
@@ -54,7 +55,7 @@ function App() {
   return (
     <Container className='my-4'>
       <Routes>
-        <Route path='/' element={<div>Home</div>} />
+        <Route path='/' element={<NoteList notes={notesWithTags} availableTags={tags}/>} />
         <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags}/>} />
         <Route path='/:id'>
           <Route index element={<div>Show</div>}/>
