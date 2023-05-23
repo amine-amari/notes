403affba58e85cac5c747e23f2d71ec3d480cd9e
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-05-23 11:38:27 +0200

    add Edit Note page

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -9,12 +9,12 @@ type NoteFormProps = {
     onSubmit: (note: NoteData) => void
     onAddTag: (tag: Tag) => void
     availableTags: Tag[]
-}
+} & Partial<NoteData>
 
-function NoteForm({onSubmit, onAddTag, availableTags}: NoteFormProps) {
+function NoteForm({onSubmit, onAddTag, availableTags, title="", markdown="", tags =[]}: NoteFormProps) {
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+    const [selectedTags, setSelectedTags] = useState<Tag[]>(tags)
     const navigate = useNavigate()
 
     function handleSubmit(e: FormEvent) {
@@ -35,7 +35,7 @@ function NoteForm({onSubmit, onAddTag, availableTags}: NoteFormProps) {
                 <Col>
                     <Form.Group controlId="title">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control ref={titleRef} required />
+                        <Form.Control ref={titleRef} required defaultValue={title}/>
                     </Form.Group>
                 </Col>
                 <Col>
@@ -65,7 +65,7 @@ function NoteForm({onSubmit, onAddTag, availableTags}: NoteFormProps) {
             </Row>
             <Form.Group controlId="markdown">
                 <Form.Label>Body</Form.Label>
-                <Form.Control ref={markdownRef} required as="textarea" rows={15}/>
+                <Form.Control ref={markdownRef} required defaultValue={markdown} as="textarea" rows={15}/>
             </Form.Group>
             <Stack direction="horizontal" gap={2} className="justify-content-end">
                 <Button type="submit" variant="primary">Save</Button>
